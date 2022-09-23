c3c4ce5b7cc3c201a4394ff804258214711e259b
SIDDARDA GOWTHAM <85551731+SIDDARADA@users.noreply.github.com>
2022-09-23 22:21:41 +0530

    Create lcm.py

---
+++ lcm.py
@@ -0,0 +1,12 @@
+def lcm(a,b):
+    lcm.multiple=lcm.multiple+b
+    if((lcm.multiple % a == 0) and (lcm.multiple % b == 0)):
+        return lcm.multiple;
+    else:
+        lcm(a, b)
+    return lcm.multiple
+lcm.multiple=0
+a=int(input())
+b=int(input())
+LCM=lcm(b,a)
+print(LCM)
